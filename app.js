469adb2dfb5f1a4e873119045cb623b584f0c6f5
Александр Ефименко <androidblog@yandex.ru>
2020-03-17 19:20:36 +0300

    вывод таблицы

--- app.js
+++ app.js
@@ -1,55 +1,44 @@
-
-
-const car = (name, model, owner, year, phone, image, date) => ({
-    name,
-    model,
-    owner,
-    year,
-    phone,
-    image,
-    date
+var row = (month, sum, mainPay, percentPay, balance) => ({
+    month,
+    sum,
+    mainPay,
+    percentPay,
+    balance
 })
-
-
-const cars = [
-    car('Ford', 'Focus', 'Max', 2016, '+7 929 123 45 67', 'focus.webp', '2019-10-04'),
-    car('Ford', 'Mondeo', 'Vladimir', 2018, '+7 900 532 35 67', 'ford.webp', '2019-10-04'),
-    car('Porshe', 'Panamera', 'Irina', 2015, '+7 214 877 00 00', 'panamera.jpg', '2009-03-10T00:00:00')
-]
-
+var tbData = [ // Массив строк
+ 
+            ]
 new Vue({
     el: '#app',
+
     data: {
-        cars: cars,
-        car: cars[0],
-        selectedCarIndex: 0,
-        phoneVisibility: false,
-        search: '',
-        modalVisibility: false,
-        hover: false
+        countMonth: 0,
+        sum: 0,
+        percent: 0,
+        term: 0,
+        dateStart: 0,
+        tbData: tbData
+    },
+    computed: { // Определяем вычисляемое свойство для автоматического пересчета сумм и итога
+
     },
     methods: {
-        selectCar: function (index) {
-            console.log('Click', index)
-            this.car = cars[index]
-            this.selectedCarIndex = index
+        fAddNewRow: function (month, sum, mainPay, percentPay, balance) { // Добавить новую строку в таблицу
+            this.tbData.push({
+                month: month,
+                sum: sum,
+                mainPay: mainPay,
+                percentPay: percentPay,
+                balance: balance
+            });
         }
-    },
-    computed: {
-        phoneBtnText() {
-            return this.phoneVisibility ? 'Hide phone' : 'Show phone'
-        },
-        filteredCars() {
-            return this.cars.filter(car => {
-                return car.name.indexOf(this.search) > -1 || car.model.indexOf(this.search) > -1
-            })
-        }
-    },
-    filters: {
-        momentDate: function (date) {
-            moment.locale('ru');
-            return moment(date).format('LL');
-        }
-
+    }
+ ,
+   watch: {
+      term: function () {
+          this.tbData.length = 0;
+          for (i = 0; i < this.term; ++i) 
+          this.fAddNewRow('01.01.2020', 100, 10, 90, 1000)
+   }
     }
 });
